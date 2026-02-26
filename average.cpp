7116198cc46c37a44683c9c8d6e1d8f291994e01
nicklovecat <skykillergamer@gmail.com>
2026-02-27 03:07:17 +0700

    lab21 completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc, char *argv[]){
+    if(argc <= 1){
+        cout << "Please input numbers to find average.\n";
+        return 0;
+    }
+    
+    double sum = 0;
+    int count = argc - 1;
+    for(int i = 1; i < argc; i++){
+        sum += atof(argv[i]);
+    }
+    
+    cout << "---------------------------------\n";
+    cout << "Average of " << count << " numbers = " << sum/count << "\n";
+    cout << "---------------------------------\n";
+    
+    return 0;
+}
